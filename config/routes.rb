ad7ea96b961f09e2a85e41fc44e3c2b28de6ec77
Maciej Pyszczak <maciejpyszczak@Mac.Dlink>
2026-06-04 23:00:13 +0200

    add registration

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "sign_up" => "registrations#new", as: :sign_up
+  post "sign_up" => "registrations#create", as: :sign_up_create
 
   # Defines the root path route ("/")
   root "home#index"
