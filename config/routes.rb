361150c9cb38cebd3dab2fc13276eea2e3394bcb
Maciej Pyszczak <maciejpyszczak@Mac.Dlink>
2026-06-04 23:15:43 +0200

    add admin dashboard and redirect based on user's role

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "home/index"
+  namespace :admin do
+    root "dashboard#index"
+  end
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
