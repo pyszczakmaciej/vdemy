f8d4d90ff0913be9efd4138c867f291952fa17d0
Maciej Pyszczak <maciejpyszczak@Mac.Dlink>
2026-06-06 22:47:51 +0200

    add my-courses page for user

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,10 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = current_user.enrolled_courses
+    @categories = Course::CATEGORY_LABELS.keys
+
+    if params[:category].present? && @categories.include?(params[:category])
+      @courses = @courses.where(category: params[:category])
+    end
+  end
+end
