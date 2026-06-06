de3c3a30effc30c4113edb22d12f3b224bc563a0
Maciej Pyszczak <maciejpyszczak@Mac.Dlink>
2026-06-06 14:24:10 +0200

    add single course preview

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,4 +2,8 @@ class DiscoverController < ApplicationController
   def index
     @courses = Course.all.where(published: true)
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
 end
