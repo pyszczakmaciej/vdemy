b7264911b82e8ffbaacffefabd841196d16d6078
Maciej Pyszczak <maciejpyszczak@Mac.Dlink>
2026-06-08 14:55:08 +0200

    fix courses empty state

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,9 +2,11 @@ class CoursesController < ApplicationController
   def index
     @courses = current_user.enrolled_courses
     @categories = Course::CATEGORY_LABELS.keys
+    @category = @categories.first
 
     if params[:category].present? && @categories.include?(params[:category])
       @courses = @courses.where(category: params[:category])
+      @category = params[:category]
     end
   end
 end
